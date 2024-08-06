2ffec721baa8bf0a2f12f790e0d596774cda68fb
Gothmane <76252610+Guennach@users.noreply.github.com>
2024-08-06 17:49:37 +0100

    CRM endpoint

--- index.js
+++ index.js
@@ -1,60 +1,91 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
-const axios = require("axios");
-var bodyParser = require("body-parser");
-
+const cors = require("cors");
+var   bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(express.json());
 
-// const checkallowedhost = (req, res, next) => {
-//   const allowedHost = ["localhost", "127.0.0.1"];
-//   const host = req.get("host").split(":")[0];
-//   if (allowedHost.includes(host)) {
-//     next();
-//   } else {
-//     res.status(403).json({ error: "Not allowed" });
-//   }
-// };
-
-// app.use(checkallowedhost);
-
-app.get("/CRMendPoint", async (req, res) => {
-  try {
-    // const response = await axios.get('http://127.0.0.1:3000/test');
-    // res.status(200).json(response.data);
-    // console.log(response);
-    res.status(200).json({ message: "Test get endpoint" });
-  } catch (error) {
-    res.status(500).json({ error: "An error occurred while fetching data" });
-  }
-});
 
 app.post("/CRMendPoint", async (req, res) => {
   try {
-    if (!req.body) res.status(400).json({ error: "No request body" });
-    const key = {
-      key: "value",
+    if (!req.body) {
+      return res.status(400).json({ error: "No request body" });
+    }
+
+    const {
+      FNAME: firstName,
+      LNAME: lastName,
+      EMAIL: email,
+      STATE: state,
+      UDF119: userIP,
+      UDF130: UDF130,
+      UDF88: UDF88,
+      UDF142: UDF142,
+      UDF153: UDF153,
+      UDF89: UDF89,
+      HOME_PHONE: phone,
+    } = req.body;
+
+
+    if (!firstName || !lastName || !email || !state || !userIP || !phone) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const leadData = {
+      FNAME: firstName,
+      LNAME: lastName,
+      EMAIL: email,
+      HOME_PHONE: phone,
+      STATE: state,
+      LEAD_PROVIDER_ID: "Weblead",
+      DISTRIBUTIONID: 21,
+      UDF130: UDF130,
+      UDF88: UDF88,
+      UDF142: UDF142,
+      UDF153: UDF153,
+      UDF89: UDF89,
+      UDF119: userIP,
+      NOTES: JSON.stringify({
+        HelpWith: "Unfiled Tax Returns Liability",
+      }),
+      apiKey: process.env.API_KEY,
     };
-    var data = { ...req.body, ...key };
-    const response = await axios.post(`${req.protocol}://${req.get('host')}/test`, data);
-    res.status(200).json(response.data);
+
+    const queryParams = new URLSearchParams(leadData).toString();
+    const crmEndpoint = `https://ideal.irslogics.com/postLead.aspx?${queryParams}`;
+    
+    fetch(crmEndpoint, {
+      method: "GET",
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.indexOf("application/json") !== -1) {
+        res.status(200).json(response.data);
+      } else {
+        return response.text().then((data) => {
+          // Parse the response text
+          const caseIDMatch = data.match(/CaseID:\s*(\d+)/);
+          const officerNameMatch = data.match(/SetOfficerName:\s*([\w\s]+)/);
+          const parsedData = {
+            message: "Lead imported successfully",
+            caseID: caseIDMatch ? caseIDMatch[1] : null,
+            officerName: officerNameMatch ? officerNameMatch[1] : null,
+          };
+          res.status(200).json(parsedData);
+        });
+      }
+    });
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching data" });
   }
 });
 
-app.post("/test", (req, res) => {
-  try {
-      res.status(200).json({ message: "Test post endpoint" });
-  } catch (error) {
-    res.status(500).json({ error: "An error occurred while fetching data" });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
