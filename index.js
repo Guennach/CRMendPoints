29f7c1206e5c52726c391689f5cf18f7dab0f914
Gothmane <76252610+Guennach@users.noreply.github.com>
2024-08-06 19:36:35 +0100

    restriction

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-require('dotenv').config(); // Load environment variables from .env file
+require("dotenv").config(); // Load environment variables from .env file
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
 
-var   bodyParser = require("body-parser");
-
+var bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +15,17 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 const checkallowedhost = (req, res, next) => {
-  const allowedHost = ["localhost", "127.0.0.1", "freshstartagents.com", "irshelpers.com", "itistaxtime.com", "cr-mend-points.vercel.app"];
+  const allowedHost = [
+    "localhost",
+    "127.0.0.1",
+    "freshstartagents.com",
+    "irshelpers.com",
+    "itistaxtime.com",
+    "cr-mend-points.vercel.app",
+  ];
+
   const host = req.get("host").split(":")[0];
+
   if (allowedHost.includes(host)) {
     next();
   } else {
@@ -27,10 +35,11 @@ const checkallowedhost = (req, res, next) => {
 
 app.use(checkallowedhost);
 
-
 app.post("/test", (req, res) => {
   try {
-    res.status(200).json({ message: `Test endpoint 200 SUCCESS ${process.env.TEST_ENV}` });
+    res
+      .status(200)
+      .json({ message: `Test endpoint 200 SUCCESS ${process.env.TEST_ENV}` });
   } catch (error) {
     res.status(500).json({ error: "An error occurred while fetching data" });
   }
@@ -38,79 +47,88 @@ app.post("/test", (req, res) => {
 
 app.post("/CRMendPoint", async (req, res) => {
   try {
-    if (!req.body) {
-      return res.status(400).json({ error: "No request body" });
-    }
-
-    const {
-      FNAME: firstName,
-      LNAME: lastName,
-      EMAIL: email,
-      STATE: state,
-      UDF119: userIP,
-      UDF130: UDF130,
-      UDF88: UDF88,
-      UDF142: UDF142,
-      UDF153: UDF153,
-      UDF89: UDF89,
-      HOME_PHONE: phone,
-    } = req.body;
-
-    if (!firstName || !lastName || !email || !state ||  !phone) {
-      return res.status(400).json({ error: "Missing required fields" });
-    }
-
-    const leadData = {
-      FNAME: firstName,
-      LNAME: lastName,
-      EMAIL: email,
-      HOME_PHONE: phone,
-      STATE: state,
-      LEAD_PROVIDER_ID: "Weblead",
-      DISTRIBUTIONID: 21,
-      UDF130: UDF130,
-      UDF88: UDF88,
-      UDF142: UDF142,
-      UDF153: UDF153,
-      UDF89: UDF89,
-      UDF119: userIP,
-      NOTES: JSON.stringify({
-        HelpWith: "Unfiled Tax Returns Liability",
-      }),
-      apiKey: process.env.API_KEY,
-    };
-
-    const queryParams = new URLSearchParams(leadData).toString();
-    const crmEndpoint = `https://ideal.irslogics.com/postLead.aspx?${queryParams}`;
-
-    axios.get(crmEndpoint)
-    .then((response) => {
-      const contentType = response.headers['content-type'];
-      if (contentType && contentType.indexOf('application/json') !== -1) {
-        res.status(200).json(response.data);
-      } else {
-        const data = response.data;
-        // Parse the response text
-        const caseIDMatch = data.match(/CaseID:\s*(\d+)/);
-        const officerNameMatch = data.match(/SetOfficerName:\s*([\w\s]+)/);
-        const parsedData = {
-          message: 'Lead imported successfully',
-          caseID: caseIDMatch ? caseIDMatch[1] : null,
-          officerName: officerNameMatch ? officerNameMatch[1] : null,
-        };
-        res.status(200).json(parsedData);
+    checkallowedhost(req, res, () => {
+      if (!req.body) {
+        return res.status(400).json({ error: "No request body" });
       }
-    })
-    .catch((error) => {
-      console.error('Fetch error:', error);
-      res.status(500).json({ error: 'An error occurred while fetching data' });
+
+      const {
+        FNAME: firstName,
+        LNAME: lastName,
+        EMAIL: email,
+        STATE: state,
+        UDF119: userIP,
+        UDF130: UDF130,
+        UDF88: UDF88,
+        UDF142: UDF142,
+        UDF153: UDF153,
+        UDF89: UDF89,
+        HOME_PHONE: phone,
+      } = req.body;
+
+      if (!firstName || !lastName || !email || !state || !phone) {
+        return res.status(400).json({ error: "Missing required fields" });
+      }
+
+      const leadData = {
+        FNAME: firstName,
+        LNAME: lastName,
+        EMAIL: email,
+        HOME_PHONE: phone,
+        STATE: state,
+        LEAD_PROVIDER_ID: "Weblead",
+        DISTRIBUTIONID: 21,
+        UDF130: UDF130,
+        UDF88: UDF88,
+        UDF142: UDF142,
+        UDF153: UDF153,
+        UDF89: UDF89,
+        UDF119: userIP,
+        NOTES: JSON.stringify({
+          HelpWith: "Unfiled Tax Returns Liability",
+        }),
+        apiKey: process.env.API_KEY,
+      };
+
+      const queryParams = new URLSearchParams(leadData).toString();
+      const crmEndpoint = `https://ideal.irslogics.com/postLead.aspx?${queryParams}`;
+
+      axios
+        .get(crmEndpoint)
+        .then((response) => {
+          const contentType = response.headers["content-type"];
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+            res.status(200).json(response.data);
+          } else {
+            const data = response.data;
+            // Parse the response text
+            const caseIDMatch = data.match(/CaseID:\s*(\d+)/);
+            const officerNameMatch = data.match(/SetOfficerName:\s*([\w\s]+)/);
+            const parsedData = {
+              message: "Lead imported successfully",
+              caseID: caseIDMatch ? caseIDMatch[1] : null,
+              officerName: officerNameMatch ? officerNameMatch[1] : null,
+            };
+            res.status(200).json(parsedData);
+          }
+        })
+        .catch((error) => {
+          console.error("Fetch error:", error);
+          res
+            .status(500)
+            .json({ error: "An error occurred while fetching data" });
+        });
     });
   } catch (error) {
     console.error("Server error:", error);
-    res.status(500).json({ error: "An error occurred while processing the request", details: error.message });
+    res
+      .status(500)
+      .json({
+        error: "An error occurred while processing the request",
+        details: error.message,
+      });
   }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
