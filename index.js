36b83cafd1e01d85790356f8c51cd4aee6c95aa7
Gothmane <76252610+Guennach@users.noreply.github.com>
2024-08-06 18:57:36 +0100

    500

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.post("/CRMendPoint", async (req, res) => {
       }
     });
   } catch (error) {
-    res.status(500).json(leadData);
+    res.status(500).json({ message: `ERROR >> ${process.env.API_KEY}` });
   }
 });
 
